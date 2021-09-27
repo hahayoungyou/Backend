b88f367aa371fb9683a6de6e72c3286ffaf5b91d
hahayoungyou <whgkdud0501@naver.com>
2021-09-28 00:04:58 +0900

    schedule CRUD

--- src/main/java/monthsago/tarvel16/Service/ScheduleService.java
+++ src/main/java/monthsago/tarvel16/Service/ScheduleService.java
@@ -1,9 +1,13 @@
 package monthsago.tarvel16.Service;
 
+import monthsago.tarvel16.Model.Board;
 import monthsago.tarvel16.Model.Schedule;
 import monthsago.tarvel16.Repository.ScheduleRepository;
+import monthsago.tarvel16.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 
 @Service
@@ -14,6 +18,24 @@ public class ScheduleService {
 
     public Schedule saveSchedule(Schedule schedule) {
         return scheduleRepository.save(schedule);
+
+    }
+    public List<Schedule> getSchedule(String no) {
+        return scheduleRepository.getSchedulelist(no);
+
+    }
+    public void deleteSchedule(Integer no) {
+         scheduleRepository.deleteById(no);
+
+    }
+
+    public void updateSchedule(Integer no,Schedule schedule) {
+        Schedule sch = scheduleRepository.findById(no)
+                .orElseThrow(() -> new ResourceNotFoundException("Not exist Board Data by no : [" + no + "]"));
+        sch.setTitle(schedule.getTitle());
+        sch.setStartdate(schedule.getStartdate());
+        sch.setEnddate(schedule.getEnddate());
+        scheduleRepository.save(sch);
 
     }
 }
